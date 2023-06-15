f5a55790108fbe286835f0302cb597879893fe9e
designwithashik <designwithashik@gmail.com>
2023-06-15 21:26:03 +0600

    Database Initial Setup Added

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const app = express()
+const cors = require('cors');
+const port = process.env.PORT || 3000
+require("dotenv").config();
+
+
+//middleware
+
+const corsOptions = {
+    origin: '*',
+    credentials: true,
+    optionSuccessStatus: 200,
+}
+
+//middleware
+app.use(express.json())
+app.use(cors(corsOptions))
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u1007ka.mongodb.net/?retryWrites=true&w=majority`;
+
+
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const database = client.db('prioroto');
+        const tasksCollection = database.collection('tasks');
+        const usersCollection = database.collection('users');
+        
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Learn a new language')
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
